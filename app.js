71ec5ca39404720f0d2ce665ce32332ae5564b16
Chris Stanton <cstanton0760@yahoo.com>
2017-03-15 11:40:35 -0500

    add port var=5000

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
-// app.js
+//app.js
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var taskRouter = require('./routes/tasks');
+var port = 5000;
 
 //middleware
 app.use(express.static('public'));
@@ -12,6 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/tasks', taskRouter);
 
-app.listen(3003, function() {
-  console.log('listening on 3003');
+app.listen(port, function() {
+  console.log('listening on localhost: ', port);
 });
